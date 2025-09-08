9382a537149e6efa88b9bc430eeac6c0250a2936
Misha Baghdasaryan <misho191524@gmail.com>
2025-09-08 14:14:32 +0400

    added Home page and Home.css files

--- mern_food_delivery/src/components/NavBar/Navbar.jsx
+++ mern_food_delivery/src/components/NavBar/Navbar.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import "./Navbar.css"
-import { assets } from "../../assets/frontend_assets/assets.js"
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/frontend_assets/assets.js";
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState("menu");
   return (
     <div className="navBar">
-        <img src={assets.logo} alt="Logo_Tomato" className="logo"/>
-        <ul className="navbar-menu">
-          <li>Home</li>
-          <li>Menu</li>
-          <li>mobile-app</li>
-          <li>contact us</li>
-         
-        </ul>
-        <div className="navbar-right">
-          <img src={assets.search_icon} alt="search_icon" />
-          <div className="navabr-search-icon">
-            <img src={assets.basket_icon} alt="basket_icon" />
-          </div>
-          <button>sign in</button>
+      <img src={assets.logo} alt="Logo_Tomato" className="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search_icon" />
+        <div className="navabar-search-icon">
+          <img src={assets.basket_icon} alt="basket_icon" />
+          <div className="dot"></div>
         </div>
+        <button>sign in</button>
+      </div>
     </div>
-  )
-}
+  );
+};
